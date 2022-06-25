4d85ddfc7051916c713537e0e2c9a1a3e01fd57f
Amrit Purandare <amrutpurandare213@gmail.com>
2022-06-25 19:25:40 +0530

    Added Blank Activity and Application class Added required permissions for Network API call Constraint layout dependency added

---
+++ app/src/main/java/com/amrit/zensarnewsapp/view/NewsHeadlinesActivity.kt
@@ -0,0 +1,12 @@
+package com.amrit.zensarnewsapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amrit.zensarnewsapp.R
+
+class NewsHeadlinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_headlines)
+    }
+}
