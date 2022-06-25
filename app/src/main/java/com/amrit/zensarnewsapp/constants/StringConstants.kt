887bdb396a45f8f90b80a30e6f82eedbce7db910
Amrit Purandare <amrutpurandare213@gmail.com>
2022-06-25 19:58:43 +0530

    Added dependencies for coroutines
    Calling API from the viewmodel. posting the proper values which will help to drive the UI

--- app/src/main/java/com/amrit/zensarnewsapp/constants/StringConstants.kt
+++ app/src/main/java/com/amrit/zensarnewsapp/constants/StringConstants.kt
@@ -2,4 +2,5 @@ package com.amrit.zensarnewsapp.constants
 
 object StringConstants {
     const val API_STATUS_OK = "ok"
+    const val API_ERROR_MESSAGE = "something went wrong. Try Again."
 }
--- app/src/main/java/com/amrit/zensarnewsapp/viewmodal/NewsViewModal.kt
+++ app/src/main/java/com/amrit/zensarnewsapp/viewmodal/NewsViewModal.kt
@@ -1,6 +1,30 @@
 package com.amrit.zensarnewsapp.viewmodal
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amrit.zensarnewsapp.constants.NetworkAPIConstants
+import com.amrit.zensarnewsapp.constants.StringConstants.API_ERROR_MESSAGE
+import com.amrit.zensarnewsapp.modal.Articles
+import com.amrit.zensarnewsapp.modal.repository.NewsRepository
+import com.amrit.zensarnewsapp.network.Response
+import kotlinx.coroutines.launch
 
 class NewsViewModal : ViewModel() {
+
+    private var newsData = MutableLiveData<Response<List<Articles>>>()
+    val newsHeadLines: LiveData<Response<List<Articles>>>
+        get() = newsData
+    
+    fun getNewsHeadline(countryCode: String = NetworkAPIConstants.COUNTRY_USA) {
+        newsData.postValue(Response.loading(null))
+        viewModelScope.launch {
+            val data = NewsRepository().fetchNewsData(countryCode)
+            if (data?.isNotEmpty() == true)
+                newsData.postValue(Response.success(data))
+            else
+                newsData.postValue(Response.error(null, API_ERROR_MESSAGE))
+        }
+    }
 }
