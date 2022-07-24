21b4543c2a173b487b3760c4ecf23e04aa2ec402
Amrit Purandare <amrutpurandare213@gmail.com>
2022-07-24 18:31:38 +0530

    Added view binding

--- app/src/main/java/com/amrit/zensarnewsapp/view/NewsListAdapter.kt
+++ app/src/main/java/com/amrit/zensarnewsapp/view/NewsListAdapter.kt
@@ -4,36 +4,34 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.amrit.zensarnewsapp.R
+import com.amrit.zensarnewsapp.databinding.ListItemHeadlineBinding
 import com.amrit.zensarnewsapp.modal.Articles
 import com.squareup.picasso.Picasso
 
 class NewsListAdapter(private val itemClick: OnNewsRowClick) :
     RecyclerView.Adapter<NewsListAdapter.NewsViewHolder>() {
 
+    private var _binding: ListItemHeadlineBinding?= null
+    private val binding get() = _binding!!
+
     var newsList = ArrayList<Articles>()
 
-    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.newsTitle)
-        val icon: ImageView = itemView.findViewById(R.id.newsImage)
-    }
+    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val inflate =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_headline, parent, false)
-        return NewsViewHolder(inflate)
+        _binding =
+            ListItemHeadlineBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        holder.title.text = newsList[position].title
+        binding.newsTitle.text = newsList[position].title
         if (!TextUtils.isEmpty(newsList[position].urlToImage))
             Picasso.with(holder.itemView.context)
                 .load(newsList[position].urlToImage)
                 .placeholder(android.R.drawable.presence_offline)
-                .into(holder.icon)
+                .into(binding.newsImage)
         holder.itemView.setOnClickListener {
             itemClick.onNewsArticleClick(newsList[position])
         }
